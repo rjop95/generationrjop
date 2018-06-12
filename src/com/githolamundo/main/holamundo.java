03db90eaf993a39ce6780b2470c617b8af01b193
rjop95 <riccardo.jop@gmail.com>
2018-06-12 11:48:12 -0500

    primera version de proyecto

---
+++ src/com/githolamundo/main/holamundo.java
@@ -0,0 +1,12 @@
+package com.githolamundo.main;
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		System.out.println("hola mundo");
+		System.out.println("hola mundo2");
+		System.out.println("hola mundo3");
+		System.out.println("hola mundo 4");
+	}
+
+}
